56bea18fdb1a531361b14de372af6c7a86d4fb3b
dmokin2022 <zapadcenter@gmail.com>
2023-07-08 06:40:42 +0300

    Модуль COM-порта

--- ComportConnector.cpp
+++ ComportConnector.cpp
@@ -1,6 +1,56 @@
 #include "ComportConnector.h"
+#include <QDebug>
 
-ComportConnector::ComportConnector(QObject *parent) : QObject(parent)
-{
+ComportConnector::ComportConnector(QObject *parent) : QObject(parent) {
 
+    timer = new QTimer();
+
+    connect(timer, &QTimer::timeout, this, [this]() {
+        output();
+    });
 }
+
+void ComportConnector::initPort(QString portName) {
+    serialPort = new QSerialPort;
+    serialPort->setPortName(portName);     // имя последовательного порта
+    serialPort->setBaudRate(COMPORT_BITRATE);    // Скорость бода
+    serialPort->setDataBits(QSerialPort::Data8);     // Data Bit.
+    serialPort->setParity(QSerialPort::NoParity);    //Контрольная цифра
+    serialPort->setStopBits(QSerialPort::OneStop);   // остановка
+    serialPort->setFlowControl(QSerialPort::NoFlowControl); // Управление потоком обычно бесполезно
+
+    // [2] последовательный порт
+    if (serialPort->open(QIODevice::ReadWrite)) {
+        qDebug()<< "Последовательный порт" << portName << "был открыт, режим чтения и записи";
+    } else {
+        qDebug()<< "Ошибка открытия серийного порта" << portName << serialPort->errorString();
+        serialPort->clearError();
+    }
+
+    // Настройка обработчика сигнала по приёму данных
+    connect(serialPort, &QSerialPort::readyRead, this, &ComportConnector::onSerialPortDataReceived);
+}
+
+void ComportConnector::onSerialPortDataReceived() {
+    if (serialPort->bytesAvailable()) {
+        timer->start(COMPORT_TIMEOUT);
+        const QByteArray recv_data = serialPort->readAll();
+        QString inputSymbols = QString::fromUtf8(recv_data);
+        qDebug() << "Получено из COM-порта:" << inputSymbols;
+
+        if (inputSymbols.contains(END_OF_STRING_SYMBOL)) {
+            inputSymbols.remove(END_OF_STRING_SYMBOL);
+            inputString.append(inputSymbols);
+            output();
+            return;
+        }
+
+        inputString.append(inputSymbols);
+    }
+}
+
+
+void ComportConnector::output() {
+    if (!inputString.isEmpty()) emit ready(inputString);
+}
+
--- ComportConnector.h
+++ ComportConnector.h
@@ -2,6 +2,14 @@
 #define COMPORTCONNECTOR_H
 
 #include <QObject>
+#include <QTimer>
+
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+#define COMPORT_TIMEOUT 5000 //миллисекунды
+#define END_OF_STRING_SYMBOL "\r"
+
 
 class ComportConnector : public QObject
 {
@@ -9,7 +17,20 @@ class ComportConnector : public QObject
 public:
     explicit ComportConnector(QObject *parent = nullptr);
 
+    void initPort(QString portName);
 signals:
+    void ready(const QString &string);
+
+public slots:
+    void onSerialPortDataReceived();
+
+private:
+    void output();
+    QSerialPort *serialPort;
+    QSerialPortInfo serialInfo;
+    QTimer *timer;
+    QString inputString;
+
 
 };
 
